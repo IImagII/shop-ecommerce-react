54e7f0af8fdc753c7b6c5d51c925f646b6ff8a23
iimagii <mag_1@ukr.net>
2023-02-22 23:41:20 +0200

    сделал меню бургер без иконок корзины

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,31 +2,37 @@ import React, { useContext, useState } from 'react'
 import { IoMdBasket } from 'react-icons/io'
 import { Orders } from './Orders'
 import { EmptyCart } from './EmptyCart'
-import { calcTotalPrice } from '../utils/calcTotalPrice'
+
 import { OrdersContext } from '../hooks/OrdersProvider'
 import { OrderShow } from './OrderShow'
+import { FiMenu } from 'react-icons/fi'
+import { GrClose } from 'react-icons/gr'
 
 export const Header = () => {
    const [cartOpen, setCartOpen] = useState(false)
    const { orders } = useContext(OrdersContext) // берем из контекста
-
-   //ждя сокращения кода и последующего его вывода
+   const [touch, setTouch] = useState(false) //состояние для показа гамбурегр меню
 
    return (
       <header>
-         <div>
-            <span className='logo'>House Staff</span>
-            <IoMdBasket
+         <div className='header'>
+            <div className='header__logo'>House Staff</div>
+
+            {/* <IoMdBasket
                onClick={() => setCartOpen(cartOpen => !cartOpen)}
                className={`shop-cart-icon ${cartOpen && 'active'}`}
-            />
-            <nav>
-               <ul className='nav'>
-                  <li>Про нас</li>
-                  <li>Контакты</li>
-                  <li>Кабинет</li>
-               </ul>
-            </nav>
+            /> */}
+
+            <ul className={`nav ${touch && 'touch'}`}>
+               <li>Про нас</li>
+               <li>Контакты</li>
+               <li>Кабинет</li>
+            </ul>
+            {/* Смена иконки при нажатии на бургер меню */}
+            <div className={`menu`} onClick={() => setTouch(touch => !touch)}>
+               {touch ? <GrClose size={25} /> : <FiMenu size={25} />}
+            </div>
+
             {/* вывод товаров непосредсвтенно в блоке с корзиной */}
             {cartOpen && (
                <div className='shop-cart'>
@@ -38,14 +44,14 @@ export const Header = () => {
                </div>
             )}
          </div>
-         <div className='order-page__right'>
+         {/* <div className='order-page__right'>
             <div className='order-page__total-price'>
                {orders.length > 0 && <span>{orders.length} шт. 6 грн.</span>}
             </div>
          </div>
          <div className='presentation'>
             <img src='@img' alt='' />
-         </div>
+         </div> */}
       </header>
    )
 }
