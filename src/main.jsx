f3fc509c4746a880e1bf16f1f4cfd23fdbd7a08a
iimagii <mag_1@ukr.net>
2023-02-23 18:30:45 +0200

    фиксю

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { OrdersProvider } from './hooks/OrdersProvider'
 import './index.scss'
-import { CountProvider } from './hooks/countOrderProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-   <CountProvider>
-      <OrdersProvider>
-         <App />
-      </OrdersProvider>
-   </CountProvider>
+   <OrdersProvider>
+      <App />
+   </OrdersProvider>
 )
