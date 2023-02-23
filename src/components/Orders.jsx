214e4f213b02cd931a0d171123cb2694f83c9ce9
iimagii <mag_1@ukr.net>
2023-02-23 13:21:54 +0200

    подправил верстку козины в header

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react'
-
 import { OrdersContext } from '../hooks/OrdersProvider'
 import {
    BsFillArrowUpSquareFill,
@@ -7,12 +6,27 @@ import {
 } from 'react-icons/bs'
 
 export const Orders = ({ ...order }) => {
-   const { id, img, title, price } = order
+   const { id, img, title, price, count } = order
    const { orders, setOrders } = useContext(OrdersContext) // берем из контекста
 
    //функция удаления товаров из корзины
    const handleRemoveOrder = id => {
       setOrders(orders.filter(order => order.id !== id))
+   }
+
+   //функция добавления большегоколичества товаров
+   const handleIncreseOrder = id => {
+      setOrders(orders => {
+         return orders.map(order => {
+            if (order.id === id) {
+               return {
+                  ...order,
+                  count: ++order.count,
+               }
+            }
+            return order
+         })
+      })
    }
    return (
       <>
@@ -23,12 +37,14 @@ export const Orders = ({ ...order }) => {
                   <img src={img} alt='title' />
                </div>
                <div>
-                  <b>Цена: {price} гривен</b>
+                  <b>Стоимость: {price * count} гривен</b>
                </div>
             </div>
-            <div className='cart__count'>кол. 1</div>
+            <div className='cart__count'>кол. {count}</div>
             <div className='cart__arrow'>
-               <BsFillArrowUpSquareFill />
+               <BsFillArrowUpSquareFill
+                  onClick={() => handleIncreseOrder(id)}
+               />
                <BsFillArrowDownSquareFill />
             </div>
             <div className='cart__delete' onClick={() => handleRemoveOrder(id)}>
